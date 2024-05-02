ac4dca1662f7a476ffb2c7f5bfab7f5227f64cf2
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2024-05-02 22:31:25 +0500

    Add files via upload

--- library/views/Dialog.js
+++ library/views/Dialog.js
@@ -1,30 +1,28 @@
 
 //------------------------------------------------
-//----------- GENIE DIALOG VIEW ------------------  UNLOGGED
+//----------- GENIE DIALOG VIEW ------------------
 var GenieDialogView = function() {
 	var ParentView;
 	var OkButton;
 };
 GenieDialogView.prototype = new GenieView();
-GenieDialogView.prototype.Set = function(cnvs, specs, tWriter, pView) {
-
-	this.SetLinks(null, this.TextWriter);
-
+GenieDialogView.prototype.Set = function(cnvs, specs, pView) {
 	GenieView.prototype.Set.call(this, cnvs, specs);
 
 	this.ParentView = pView;
 };
 GenieDialogView.prototype.SetControls = function() {
 
-	this.OkButton = new GenieText();
-	this.OkButton.Set(this.Canvas, this.Specs, this.TextWriter);
+	this.OkButton = new TextButton();
+	this.OkButton.Set(this.Canvas, this.Specs.BUTTON.OK, this.TextWriter);
+	this.OkButton.SetCornersPic(RoundedCornerImages);
 	this.Controls.push(this.OkButton);
 };
 GenieDialogView.prototype.Open = function() {
 
 	this.ParentView.Disable();
-	this.Draw();
-	this.OkButton.Show();
+
+	GenieView.prototype.Open.call(this);
 };
 GenieDialogView.prototype.Update = function() {
 
@@ -37,6 +35,12 @@ GenieDialogView.prototype.Update = function() {
 };
 GenieDialogView.prototype.Close = function() {
 
-	this.OkButton.Disable();
+	this.OkButton.Hide();
 	this.ParentView.Open();
+	this.ParentView.Update();
+};
+GenieDialogView.prototype.ColourScape = function() {
+
+	this.Context.fillStyle = this.Specs.COLOUR;
+	this.Context.fillRect(this.Specs.L, this.Specs.T, this.Specs.W, this.Specs.H);
 };
