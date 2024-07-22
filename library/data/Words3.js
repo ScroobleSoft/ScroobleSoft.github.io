b54b47267415ce0cbfcc58279256089b257b9ce2
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2024-07-22 20:37:10 +0500

    Add files via upload

---
+++ library/data/Words3.js
@@ -0,0 +1,6 @@
+//NOTE: none that start with q or end with x
+var Words3 = [ "ace", "act", "add", "ado", "age", "ago", "aid", "aim", "air", "ale", "all", "and", "ant", "any", "apt", "arc", "are", "ark", "arm", "art",
+					"ash", "ask", "asp", "ass", "ate", "awe", "axe",
+					"bad", "bag", "ban", "bar", "bat", "bay", "bed", "bee", "beg", "bet", "bid", "big", "bin", "boa", "bog", "bow", "boy", "bud", "bug", "bum",
+					"bun", "bus", "but", "buy", "bye"
+];
