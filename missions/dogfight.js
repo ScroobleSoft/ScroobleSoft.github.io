5a61994254a4a6f08ad795e97ced09a83ee0bebb
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-04-02 17:38:59 +0500

    Delete missions directory

--- missions/dogfight.js
+++
@@ -1,9 +0,0 @@
-/*
- *  UNLOGGED
- */
-//---------------------------------------------------------
-//---------- DOMINION DOGFIGHT THEATRE --------------------
-var DominionDogfightTheatre = function() {
-   //-need only fighters and interceptors
-};
-DominionDogfightTheatre.prototype = new DominionAirTheatre();
