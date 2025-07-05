e4d72b902e376376e7b8138ddf205e748b83b3fc
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-05 22:47:02 +0500

    Add files via upload

--- freebooter/views/intro/info.js
+++ freebooter/views/intro/info.js
@@ -8,23 +8,7 @@ SolarIntroInfoView.prototype.Set = function(cnvs, specs, mView) {
 	GenieSubView.prototype.Set.call(this, cnvs, specs, mView);
 
 };
-/*
-SolarIntroInfoView.prototype.SetControls = function() {
-
-	if (Game.CheckMobile()) {
-		this.InstantButton = this.SetTextButton(this.Specs.BUTTON.INSTANT, RaisedCornerImages, this.TextWriter);
-		this.SubsButton = this.SetTextButton(this.Specs.BUTTON.SUBSTITUTION, RaisedCornerImages, this.TextWriter);
-		this.PauseButton = this.SetTextButton(this.Specs.BUTTON.PAUSE, RaisedCornerImages, this.TextWriter);
-	}
-};
-*/
-/*
-SolarIntroInfoView.prototype.Open = function() {  //UNLOGGED
-	GenieSubView.prototype.Open.call(this);
-
-};
-*/
-SolarIntroInfoView.prototype.Update = function() {  //UNLOGGED
+SolarIntroInfoView.prototype.Update = function() {
 
 	SolarSystem.Draw();
 	SolarSystem.Update();
