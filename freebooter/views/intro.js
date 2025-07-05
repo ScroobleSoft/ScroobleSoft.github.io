7fb7aebb286e720ad9c226fe155049247f5b4498
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-05 22:43:28 +0500

    Add files via upload

--- freebooter/views/intro.js
+++ freebooter/views/intro.js
@@ -8,49 +8,13 @@ SolarIntroView.prototype.Set = function(cnvs, specs) {
 	GenieView.prototype.Set.call(this, cnvs, specs);
 
 };
-/*  UNLOGGED
-SolarIntroView.prototype.SetControls = function() {
-
-	if (Game.CheckMobile())
-		this.SetMobileControls();
-	else {
-		this.SetSpinControls();
-		this.SetPaginations();
-		this.SetAllTouchBars();
-		this.SetDTouchBars();
-		this.SetMTouchBars();
-		this.SetATouchBars();
-	}
-};
-*/
-/*  UNLOGGED
-SolarIntroView.prototype.SetMobileControls = function() {
-
-	//Pagination
-	this.MobilePaginationImage = new GenieImage();
-	this.MobilePaginationImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.CONTROLS], this.Specs.PAGINATION.MOBILE.IMAGE);
-	this.MobilePagination = new GeniePagination();
-	this.MobilePagination.Set(this.Canvas, this.Specs.PAGINATION.MOBILE, this.Specs.PAGINATION.MOBILE.IMAGE);
-	this.Controls.push(this.MobilePagination);
-
-	//Buttons
-	this.BuyButton = new ImageButton();
-	this.BuyButton.Set(this.Canvas, this.Specs.BUTTON.BUY, ImageManager.Pics[IMAGeINDEX.CONTROLS]);
-	this.BuyButton.SetCornersPic(RaisedCornerImages);
-	this.Controls.push(this.BuyButton);
-	this.SellButton = new ImageButton();
-	this.SellButton.Set(this.Canvas, this.Specs.BUTTON.SELL, ImageManager.Pics[IMAGeINDEX.CONTROLS]);
-	this.SellButton.SetCornersPic(RaisedCornerImages);
-	this.Controls.push(this.SellButton);
-};
-*/
-SolarIntroView.prototype.Open = function() {  //UNLOGGED
+SolarIntroView.prototype.Open = function() {
 	GenieView.prototype.Open.call(this);
 
 	Starfield.Generate();
 	this.Update();
 };
-SolarIntroView.prototype.Update = function() {  //UNLOGGED
+SolarIntroView.prototype.Update = function() {
 
 	this.AnimationFrameHandle = requestAnimationFrame(this.Update.bind(this));
 
@@ -60,7 +24,7 @@ SolarIntroView.prototype.Update = function() {  //UNLOGGED
 	this.InfoView.Update();
 	this.ConsoleView.Update();
 };
-SolarIntroView.prototype.CloseAll = function() {  //Mobile only, possibly . . . UNLOGGED
+SolarIntroView.prototype.CloseAll = function() {  //TODO: Mobile only, possibly
 
 	GenieView.prototype.Close.call(this, this.OpenDockedView.bind(this), 100);
 };
--- freebooter/views/voyage.js
+++ freebooter/views/voyage.js
@@ -3,7 +3,7 @@
 //---------- SOLAR VOYAGE VIEW --------------------
 var SolarVoyageView = function() {
 	var DestinationPlanet;
-	var State;
+	var Speed, State;
 
 	var i;
 };
@@ -11,6 +11,7 @@ SolarVoyageView.prototype = new GenieView();
 SolarVoyageView.prototype.Set = function(cnvs, specs) {
 	GenieView.prototype.Set.call(this, cnvs, specs);
 
+	this.Speed = VOYAGE.SPEED.STOPPED;
 	this.State = this.Specs.STATE.LAUNCHING;
 };
 SolarVoyageView.prototype.SetDestination = function(dstntn) {  //UNLOGGED
@@ -33,14 +34,29 @@ SolarVoyageView.prototype.Update = function() {  //UNLOGGED
 		Starfield.Accelerate(0.01);
 		if (Starfield.Speed>=2.0)
 			this.State = this.Specs.STATE.TRAVELLING;
+		else if (Math.ceil(Starfield.Speed/0.25)!=this.InfoView.SpeedStep) {
+			this.InfoView.SpeedStep = Math.ceil(Starfield.Speed/0.25);
+			this.InfoView.DisplaySpeed();
+		}
 	}
+	Journey.Update();
 	Starfield.Draw();
 	Cockpit.Draw();
 	Starfield.Update();
-	this.InfoView.Update();
-	this.ConsoleView.Update();
+	if (this.State==this.Specs.STATE.TRAVELLING) {
+		this.InfoView.Update();
+		this.ConsoleView.Update();
+	}
+
+	if (Mouse.CheckLeftClicked(CANVAS.PRIME)) {
+	} else if (Mouse.CheckLeftClicked(CANVAS.ZOOM))
+		this.InfoView.UpdateClick();
+	else if (Mouse.CheckLeftClicked(CANVAS.CONSOLE))
+		this.ConsoleView.UpdateClick();
+	else
+		Mouse.ClearAll();
 };
-SolarVoyageView.prototype.LaunchWordGames = function() {  //UNLOGGED
+SolarVoyageView.prototype.LaunchWordGames = function() {
 
 	this.InfoView.Close();
 	this.ConsoleView.Close();
@@ -48,4 +64,17 @@ SolarVoyageView.prototype.LaunchWordGames = function() {  //UNLOGGED
 	this.ConsoleView = WordsConsoleView;
 	this.InfoView.Open();
 	this.ConsoleView.Open();
+};
+SolarVoyageView.prototype.CloseWordGames = function() {
+
+	this.Canvas.SuspendInput();
+	this.InfoView.State = VIEW.WORDS.STATE.INTRO;
+	this.InfoView.Close();
+	this.ConsoleView.Close();
+	this.InfoView = CockpitInfoView;
+	this.ConsoleView = CockpitConsoleView;
+	this.InfoView.Open();
+	this.ConsoleView.Open();
+	Mouse.ClearAll();
+	this.Canvas.ResumeInput();
 };
