f1efd38feff1e073534abbcb8a3f43f1da1aa45a
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-05 22:37:30 +0500

    Add files via upload

--- freebooter/data/images.js
+++ freebooter/data/images.js
@@ -7,3 +7,6 @@ var ByIMAGE = { L: 272, T: 37, W: 30, H: 29 };
 var SCROOBLESOFtIMAGE = { L: 61, T: 62, W: 178, H: 23 };
 var TRIXSTErIMAGE = { L: 61, T: 87, W: 278, H: 17 };
 var SOUNdIMAGE = { L: 61, T: 106, W: 208, H: 19 };
+
+var SHIpIMAGE = { L: 1, T: 220, W: 24, H: 13 };
+var STATIOnIMAGE = { L: 27, T: 220, W: 25, H: 25 };
--- freebooter/data/views.js
+++ freebooter/data/views.js
@@ -22,12 +22,14 @@ var VIEW = { INTRO: { INFO: { },
 																  BACK: { L: 1, T: 198, W: 31, H: 9, X: 62, Y: 174 },
 																  LEFT: { L: 58, T: 154, W: 7, H: 42, X: 21, Y: 104 },
 																  CARGO: { L: 1, T: 209, W: 39, H: 9, X: 58, Y: 115 }
-																}
+																},
+													  SPEED: { L: 150, T: 223, W: 64, H: 19, X: 163, Y: 4 }
 													},
-										  BUTTON: {	PILOTS: { },
-														HELP: { L: 157, T: 102, W: 75, H: 44, LW: 3, SX: 2, SY: 191, STYLE: BUTTON.STYLE.RAISED },
+										  BUTTON: {	HELP: { L: 157, T: 102, W: 75, H: 44, LW: 3, SX: 2, SY: 191, STYLE: BUTTON.STYLE.RAISED },
 														OPTIONS: { L: 157, T: 146, W: 75, H: 44, LW: 3, SX: 2, SY: 151, STYLE: BUTTON.STYLE.RAISED },
-														EXIT: { L: 157, T: 190, W: 75, H: 44, LW: 3, SX: 144, SY: 311, STYLE: BUTTON.STYLE.RAISED }
+														EXIT: { L: 157, T: 190, W: 75, H: 44, LW: 3, SX: 144, SY: 311, STYLE: BUTTON.STYLE.RAISED },
+														PLUS: { L: 157, T: 52, W: 30, H: 30, LW: 3, SX: 73, SY: 205, STYLE: BUTTON.STYLE.RAISED },
+														MINUS: { L: 202, T: 52, W: 30, H: 30, LW: 3, SX: 99, SY: 205, STYLE: BUTTON.STYLE.RAISED }
 													 }
 										},
 							   CONSOLE: { COLOUR: SOLArCOLOUR.COCKPIT.LIVID,
@@ -82,9 +84,6 @@ var VIEW = { INTRO: { INFO: { },
 										},
 							 INFO: { },
 							 CONSOLE: { COLOUR: SOLArCOLOUR.COCKPIT.LIVID,
-											IMAGE: { SHIP: { L: 1, T: 220, W: 24, H: 13 },
-														STATION: { L: 27, T: 220, W: 25, H: 25 }
-													 },
 											BUTTON: { LAUNCH: { L: 79, T: 208, W: 75, H: 30, LW: 3, SX: 2, SY: 118, STYLE: BUTTON.STYLE.RAISED }
 													  }
 									    }
@@ -94,21 +93,35 @@ var VIEW = { INTRO: { INFO: { },
 							  INFO: { },
 							  CONSOLE: { }
 							},
-				 WORDS: { CELL: { STATE: { CLEAR: 0, INCORRECT: 1, CLOSE: 2, CORRECT: 3 } }, 
+				 WORDS: { STATE: { INTRO: 0, GAME: 1, SOLVED: 2 },
 							 INFO: { COLOUR: SOLArCOLOUR.COCKPIT.LIVID,
-										GRID: { CELLS: 25, C: 5, R: 5, W: 41, H: 41, LW: 2 },
+										TYPE: { LEDGER: 0, VOWELS: 1, CORNERS: 2, RAKE: 3 },
+										CELL: { W: 41, H: 41, LW: 2, O: { X: 10, Y: 6 }, STATE: { CLEAR: 0, INCORRECT: 1, CLOSE: 2, CORRECT: 3 } },
+										GRID: { CELLS: 25, C: 5, R: 5, T: 21, L: 8, W: 197, H: 197, CW: 37, CH: 37 },
+										LIGHT: { GREEN: "rgb(175,207,111)", AMBER: "rgb(255,207,015)", RED: "rgb(239,063,079)" },
+										COLOURS: [ "rgb(185,229,255)", "rgb(159,223,255)", "rgb(111,207,255)", "rgb(047,191,255)", "rgb(000,175,255)" ],
 										IMAGE: { LETTERS: { L: 61, T: 2, W: 308, H: 60, O: 4, C: 13, R: 2, PATCH: { W: 20, H: 28 } },
-													CELL: { L: 328, T: 205, W: 41, H: 41 },
-													SUbCELL: { L: 328, T: 248, W: 37, H: 37 },
-													SELECTION: { L: 328, T: 287, W: 33, H: 33 }
+													CELL: { L: 297, T: 106, W: 41, H: 41 },
+													SUbCELL: { L: 297, T: 149, W: 37, H: 37 },
+													SELECTION: { L: 297, T: 188, W: 37, H: 37 },
+													SOLUTIONS: { L: 72, T: 223, W: 76, H: 16, X: 70, Y: 3 },
+													SOLARONS: { L: 1, T: 294, W: 207, H: 16, X: 2, Y: 221 }
+												 },
+										BUTTON: { LEDGER: { L: 23, T: 23, W: 86, H: 86, LW: 3, SX: 647, SY: 1, STYLE: BUTTON.STYLE.RAISED },
+													 VOWELS: { L: 131, T: 23, W: 86, H: 86, LW: 3, SX: 647, SY: 83, STYLE: BUTTON.STYLE.RAISED },
+													 CORNERS: { L: 23, T: 131, W: 86, H: 86, LW: 3, SX: 647, SY: 165, STYLE: BUTTON.STYLE.RAISED },
+													 RAKE: { L: 131, T: 131, W: 86, H: 86, LW: 3, SX: 647, SY: 247, STYLE: BUTTON.STYLE.RAISED }
 												 }
 									  },
 							 CONSOLE: { COLOUR: SOLArCOLOUR.COCKPIT.LIVID,
-											IMAGE: { BUTTON: { L: 294, T: 64, W: 66, H: 32, O: 2, C: 2, R: 1, PATCH: { W: 32, H: 32 } },
+											KEY: { COUNT: 26, W: 31, H: 31, C: 5, R: 6, O: { X: 8, Y: 5 } },
+											IMAGE: { BUTTON: { L: 294, T: 64, W: 64, H: 31, O: 2, C: 2, R: 1, PATCH: { W: 31, H: 31 } },
 														LETTERS: { L: 61, T: 64, W: 231, H: 45, O: 3, C: 13, R: 2, PATCH: { W: 15, H: 21 } }
 													 },
-											 BUTTON: { EXIT: { L: 98, T: 208, W: 58, H: 32, LW: 3, SX: 1, SY: 1, STYLE: BUTTON.STYLE.RAISED }
-														}
+											BUTTON: { HELP: { L: 0, T: 208, W: 52, H: 28, LW: 3, SX: 97, SY: 1, STYLE: BUTTON.STYLE.RAISED },
+														 NEW: { L: 52, T: 208, W: 52, H: 28, LW: 3, SX: 49, SY: 1, STYLE: BUTTON.STYLE.RAISED },
+														 EXIT: { L: 104, T: 208, W: 52, H: 28, LW: 3, SX: 1, SY: 1, STYLE: BUTTON.STYLE.RAISED }
+													  }
 										  }
 							}
 };
