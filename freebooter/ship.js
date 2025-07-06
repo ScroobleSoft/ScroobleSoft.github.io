8363ee99eff2a52b7520a353bd106f6fffb046d6
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-06 15:18:47 +0500

    Add files via upload

--- freebooter/ship.js
+++ freebooter/ship.js
@@ -1,21 +1,17 @@
 
-/***** NOTE: below is probably what was going to be used for NPC ships in 3D view */
+//-----------------------------------------------
+//---------- FREEBOOTER SHIP --------------------
+var FreebooterShip = function() {
+	var Pilot;
+	var Speed;
+	var Armament;
+};
+FreebooterShip.prototype = {
+	Set() {
+		//-set armament slots
+	},
+	SetPilot(pilot) {
 
-//-------------------------------------------------
-//---------- SOLAR BOUNTY SHIP --------------------
-var SolarBountyShip = function() {
-   var Vertices;
-   var ScaledVertices;
-};
-SolarBountyShip.prototype = new GenieAgent();
-SolarBountyShip.prototype.Set = function(pos, dstn, drctn, specs, sprite, unit) {
-   GenieAgent.prototype.Set.call(this, pos, dstn, drctn, specs, sprite, unit);
-};
-SolarBountyShip.prototype.SetLinks = function(gTool) {
-   this.GraphicsTool = gTool;
-};
-SolarBountyShip.prototype.Update = function() {
-   //get scaled vertices based on distance
-};
-SolarBountyShip.prototype.Draw = function() {
+		this.Pilot = pilot;
+	}
 };
