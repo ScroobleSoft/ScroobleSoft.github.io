0394cebdabac3b927af5a03798682d6e065e48a4
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-06 20:41:07 +0500

    Add files via upload

--- freebooter/views/trade/console.js
+++ freebooter/views/trade/console.js
@@ -4,7 +4,6 @@
 //--------------------------------------------------------
 //---------- SOLAR TRADE CONSOLE VIEW --------------------
 var SolarTradeConsoleView = function() {
-	var ShipImage, StationImage;
 	var LaunchButton;
 	var Route;
 };
@@ -14,11 +13,6 @@ SolarTradeConsoleView.prototype.Set = function(cnvs, specs, mView) {
 
 	this.Route = new SolarRoute();
 	this.Route.Set(this.GraphicsTool);
-};
-SolarTradeConsoleView.prototype.SetImages = function() {
-
-	this.ShipImage = this.SetImage(ImageManager.Pics[IMAGeINDEX.IMAGES], this.Specs.IMAGE.SHIP);
-	this.StationImage = this.SetImage(ImageManager.Pics[IMAGeINDEX.IMAGES], this.Specs.IMAGE.STATION);
 };
 SolarTradeConsoleView.prototype.SetControls = function() {
 
@@ -36,7 +30,8 @@ SolarTradeConsoleView.prototype.Open = function() {  //UNLOGGED
 SolarTradeConsoleView.prototype.Update = function() {
 
 	if (this.LaunchButton.CheckClicked()) {
-		VoyageView.SetDestination(Planets[this.MainView.VoyagePlanets[(2*this.MainView.VoyageSelected)+1]]);
+		VoyageView.SetDestination(Planets[this.MainView.VoyagePlanets[this.MainView.VoyageSelected]]);
+		Journey.SetLocations(this.MainView.Planet, Planets[this.MainView.VoyagePlanets[this.MainView.VoyageSelected]]);
 		this.MainView.CloseAll();
 	}
 };
@@ -44,7 +39,7 @@ SolarTradeConsoleView.prototype.Draw = function() {  //UNLOGGED
 
 	this.Route.Display();
 
-	//TEMP only - locations change dynamically
-	this.ShipImage.Draw(8, 188);
-	this.StationImage.Draw(119, 7)
+	//TEMP only - locations change dynamically . . . actually, will draw a red metre to show how close
+	ShipImage.Draw(8, 188);
+	StationImage.Draw(119, 7)
 };
