0afacbf765da27f2f884a3d73c89c44445f767cd
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-07-19 20:55:21 +0500

    Add files via upload

--- library/data/Images.js
+++ library/data/Images.js
@@ -30,6 +30,10 @@ var LEDDigits = [ [0,1,2,3,4,5],	//0
 var HORIZONTAlLEDiMAGE = { L: 1, T: 195, W: 44, H: 5, O: 1, R: 1, C: 3, PATCH: { W: 14, H: 5 } };
 var VERTICAlLEDiMAGE = { L: 1, T: 201, W: 17, H: 14, O: 1, R: 1, C: 3, PATCH: { W: 5, H: 14 } };
 var DOtLEDiMAGE = { L: 195, T: 216, W: 23, H: 7, O: 1, R: 1, C: 3, PATCH: { W: 7, H: 7 } };
+var LETTERs5x7IMAGEs = { L: 1, T: 216, W: 155, H: 7, O: 1, R: 1, C: 26, PATCH: { W: 5, H: 7 } };
+var LETTERs10x14IMAGEs = { L: 1, T: 235, W: 155, H: 29, O: 2, R: 2, C: 13, PATCH: { W: 10, H: 14 } };
+var LETTERs5x5IMAGEs = { L: 1, T: 267, W: 180, H: 5, O: 2, R: 1, C: 26, PATCH: { W: 5, H: 5 } };
+
 
 //-- DIGITS --
 
@@ -37,3 +41,6 @@ var SMALlDIGITsIMAGE = { L: 46, T: 209, W: 49, H: 6, O: 1, C: 10, R: 1, PATCH: {
 var MEDIUmDIGITsIMAGE = { L: 1, T: 224, W: 69, H: 10, O: 1, C: 10, R: 1, PATCH: { W: 6, H: 10 } };
 var ICENiDIGItIMAGEs = { L: 72, T: 225, W: 78, H: 8, O: 2, C: 10, R: 1, PATCH: { W: 6, H: 8 } };
 var ARVERNiDIGItIMAGEs = { L: 21, T: 274, W: 78, H: 10, O: 2, C: 10, R: 1, PATCH: { W: 6, H: 10 } };
+var DIGITs12x13IMAGEs = { L: 46, T: 196, W: 129, H: 13, O: 2, R: 1, C: 10, PATCH: { W: 12, H: 13 } };
+var DIGITs4x6IMAGEs = { L: 46, T: 209, W: 49, H: 6, O: 1, R: 1, C: 10, PATCH: { W: 4, H: 6 } };
+var DIGITs6x10IMAGEs = { L: 1, T: 224, W: 69, H: 10, O: 1, R: 1, C: 26, PATCH: { W: 6, H: 10 } };
