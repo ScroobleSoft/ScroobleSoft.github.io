bdbfb3b0bbe396257f1469b752a38f279228c85e
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-08-16 22:16:46 +0500

    Add files via upload

--- dominion/intro.js
+++ dominion/intro.js
@@ -1,68 +1,208 @@
-
-//---------------------------------------------
-//---------- DOMINION INTRO -------------------
-var DominionIntro = function() {
+/*
+	NOTE: whether male or female head of state is shown first is determined randomly
+*/
+//----------------------------------------------------
+//---------- DOMINION CHOICE VIEW --------------------
+var DominionIntroView = function() {
+	var DailyButton, FreeFormButton, MultiChoiceButton, PlayButton, InfoButton;
+	var PickMaleButton, ModifyMaleButton, PickFemaleButton, ModifyFemaleButton;
+	var ShortButton, MediumButton, LongButton;
+	var PastButton;
+	var OkButton, CancelButton;
+	var FemaleFirstFlag, PastGamesFlag, LeaderPickedFlag;
+	var MaleX, FemaleX;
+	var MaleName, FemaleName, MaleProfile, FemaleProfile;
+	var InfoCount;
 };
-DominionIntro.prototype = new GenieIntro();
-DominionIntro.prototype.Start = function() {
+DominionIntroView.prototype = new GenieView();
+DominionIntroView.prototype.Set = function(cnvs, specs) {
+	GenieView.prototype.Set.call(this, cnvs, specs);
 
-	this.Screen.fillStyle = PAINT.SKY;
-	this.Screen.fillRect(0, 0, SCREEN.WIDTH, SCREEN.HEIGHT);
-	this.DrawJets();
-
-	this.ControlPanel.fillStyle = PAINT.SEA;
-	this.ControlPanel.fillRect(0, 0, CONTROlPANEL.WIDTH, CONTROlPANEL.HEIGHT);
-	if (!Game.CheckMobile())
-		this.SetSlideShows();
-
-	GenieIntro.prototype.Start.call(this);
-
-	this.Play();
+	this.State = this.Specs.STATE.OPEN;
+	this.InfoCount = 0;
 };
-DominionIntro.prototype.Play = function() {
+DominionIntroView.prototype.SetControls = function() {
 
-	this.AnimationFrameHandle = requestAnimationFrame(this.Play.bind(this));
+	//Choice
+	this.DailyButton = this.SetTextButton(this.Specs.BUTTON.DAILY, RaisedCornerImages, this.TextWriter);
+	this.FreeFormButton = this.SetTextButton(this.Specs.BUTTON.FREeFORM, RaisedCornerImages, this.TextWriter);
+	this.MultiChoiceButton = this.SetTextButton(this.Specs.BUTTON.MULTiCHOICE, RaisedCornerImages, this.TextWriter);
+	this.PlayButton = this.SetTextButton(this.Specs.BUTTON.PLAY, RaisedCornerImages, this.TextWriter);
+	this.InfoButton = this.SetTextButton(this.Specs.BUTTON.INFO, RaisedCornerImages, this.TextWriter);
 
-	NewsCarousel.Update();
-}
-DominionIntro.prototype.SetSlideShows = function() {
-	var i;
+	//Head of state
+	this.PickMaleButton = this.SetTextButton(this.Specs.BUTTON.PICkMALE, RaisedCornerImages, this.TextWriter);
+	this.ModifyMaleButton = this.SetTextButton(this.Specs.BUTTON.MODIFyMALE, RaisedCornerImages, this.TextWriter);
+	this.PickFemaleButton = this.SetTextButton(this.Specs.BUTTON.PICkFEMALE, RaisedCornerImages, this.TextWriter);
+	this.ModifyFemaleButton = this.SetTextButton(this.Specs.BUTTON.MODIFyFEMALE, RaisedCornerImages, this.TextWriter);
 
-	this.Ticker.fillStyle = "red";
-	this.Ticker.fillRect(0, 0, TICKER.WIDTH, TICKER.HEIGHT),
-	this.Tabloid.fillStyle = PAINT.SEA;
-	this.Tabloid.fillRect(0, 0, TABLOID.WIDTH, TABLOID.HEIGHT),
-	NewsCarousel.Show();
-	for (i=0;i<IntroInfo.length;++i)
-		NewsCarousel.AddEntry(IntroInfo[i][0], IntroInfo[i][1]);
+	//Game Options
+	this.ShortButton = this.SetTextButton(this.Specs.BUTTON.SHORT, RaisedCornerImages, this.TextWriter);
+	this.MediumButton = this.SetTextButton(this.Specs.BUTTON.MEDIUM, RaisedCornerImages, this.TextWriter);
+	this.LongButton = this.SetTextButton(this.Specs.BUTTON.LONG, RaisedCornerImages, this.TextWriter);
+	this.PastButton = this.SetTextButton(this.Specs.BUTTON.PAST, RaisedCornerImages, this.TextWriter);
+
+	//Profile
+	this.OkButton = this.SetTextButton(this.Specs.BUTTON.OK, RaisedCornerImages, this.TextWriter);
+	this.CancelButton = this.SetTextButton(this.Specs.BUTTON.CANCEL, RaisedCornerImages, this.TextWriter);
 };
-DominionIntro.prototype.DrawJets = function() {
-	var i;
-	var x, y;
+DominionIntroView.prototype.ShowControls = function() {
 
-	//Power jets
-	for (i=0;i<LeftJetSprites.length;++i) {
-		LeftJetSprites[i].Shapes.forEach(function(shape){shape.ResetSides(4+i);});
-		this.ReColourSprite(LeftJetSprites[i], i);
-		x = 213 * ((i % 3)+0.5);
-		y = 213 * (Math.floor(i/3)+0.5);
-		LeftJetSprites[i].Draw(x, y);
+	switch (this.State) {
+		case this.Specs.STATE.OPEN:
+			this.DailyButton.Show();
+			this.FreeFormButton.Show();
+			this.MultiChoiceButton.Show();
+			this.InfoButton.Show();
+			break;
+		case this.Specs.STATE.START:
+			this.PickMaleButton.Show();
+			this.ModifyMaleButton.Show();
+			this.PickFemaleButton.Show();
+			this.ModifyFemaleButton.Show();
+			this.MoveTurnButtons();
+			this.ShortButton.Show();
+			this.MediumButton.Show();
+			this.LongButton.Show();
+			if (!this.LeaderPickedFlag) {
+				this.ShortButton.Disable();
+				this.MediumButton.Disable();
+				this.LongButton.Disable();
+			}
+			this.PastButton.Show();
+			break;
+		case this.Specs.STATE.INFO:
+			this.PlayButton.Show();
+			this.InfoButton.Show();
+			break;
+		case this.Specs.STATE.CHARACTER:
+			this.OkButton.Show();
+			this.CancelButton.Show();
+	}
+};
+/*
+DominionIntroView.prototype.Open = function() {  //UNLOGGED
+
+	this.Draw();
+	this.ShowControls();
+};
+*/
+DominionIntroView.prototype.Update = function() {
+
+	this.AnimationFrameHandle = requestAnimationFrame(this.Update.bind(this));
+
+	this.UpdatePlayButtons();
+	if (this.State!=VIEW.INTRO.STATE.CHARACTER)
+		this.UpdateInfoButton();
+	if (this.State==VIEW.INTRO.STATE.START) {
+		this.UpdateProfileButtons();
+		this.UpdatePastButton();
+		this.UpdateTurnButtons();
+	}
+	if (this.State==VIEW.INTRO.STATE.CHARACTER)
+		this.UpdateCharacterControls();
+};
+DominionIntroView.prototype.Draw = function() {
+
+	this.GraphicsTool.DrawRectangle(80, 80, 240, 240, "white", 3);
+
+	this.TextWriter.SetColour("white");
+	this.TextWriter.Write("Welcome to the planet Dominion,", 96, 105);
+	this.TextWriter.Write("colonized by Dominion Corp. The", 96, 122);
+	this.TextWriter.Write("company has decided to quit the", 96, 139);
+	this.TextWriter.Write("colony, and leave its 101 nations", 96, 156);
+	this.TextWriter.Write("to pick a leader themselves.", 96, 173);
+	this.TextWriter.ResetColour();
+
+	this.Update();
+};
+DominionIntroView.prototype.DisplayInfo = function() {
+
+	this.GraphicsTool.DrawRectangle(80, 50, 240, 300, this.Specs.COLOUR, 0);
+	this.GraphicsTool.DrawRectangle(80, 50, 240, 300, "white", 3);
+
+	this.TextWriter.SetColour("white");
+
+	this.TextWriter.WriteParagraphs(IntroInfo[this.InfoCount], 87, 68, null, null, 6)
+
+	this.TextWriter.ResetColour();
+};
+DominionIntroView.prototype.SetProfiles = function() {
+
+	if (this.Randomizer.CheckBoolean()) {		//male first
+		this.MaleX = 70;
+		this.FemaleX = 210;
+	} else {												//female first
+		this.FemaleFirstFlag = true;
+		this.MaleX = 210;
+		this.FemaleX = 70;
+	}
+};
+DominionIntroView.prototype.DisplayProfiles = function() {
+	var x;
+	var date;
+
+	this.GraphicsTool.DrawRectangle(50, 50, 300, 300, this.Specs.COLOUR, 0);
+	this.GraphicsTool.DrawRectangle(50, 50, 300, 300, "white", 3);
+
+	//Male
+	CharacterGenerator.GenerateMaleProfile();
+	this.MaleProfile = CharacterGenerator.Profile;
+	CharacterGenerator.SetNation(PlayerPower);
+	this.GraphicsTool.DrawRectangle(this.MaleX, 70, 100, 100, "rgb(175,239,255)", 0);
+	this.GraphicsTool.DrawRectangle(this.MaleX-3, 67, 106, 106, "black", 3);
+	CharacterGenerator.Draw(this.MaleX, 70);
+	this.MaleName = CharacterGenerator.GenerateName() + " " + CharacterGenerator.GenerateName();
+	x = (106-StringUtils.GetTextWidth(this.MaleName, null, this.Context))/2;
+	this.TextWriter.Write(this.MaleName, this.MaleX+x-3, 188, { COLOUR: "white" } );
+
+	//Female
+	CharacterGenerator.GenerateFemaleProfile();
+	this.FemaleProfile = CharacterGenerator.Profile;
+	CharacterGenerator.SetNation(PlayerPower);
+	this.GraphicsTool.DrawRectangle(this.FemaleX, 70, 100, 100, "rgb(175,239,255)", 0);
+	this.GraphicsTool.DrawRectangle(this.FemaleX-3, 67, 106, 106, "black", 3);
+	CharacterGenerator.Draw(this.FemaleX, 70);
+	this.FemaleName = CharacterGenerator.GenerateName() + "a " + CharacterGenerator.GenerateName();
+	x = (106-StringUtils.GetTextWidth(this.FemaleName, null, this.Context))/2;
+	this.TextWriter.Write(this.FemaleName, this.FemaleX+x-3, 188, { COLOUR: "white" } );
+
+	//Daily game info
+	if (Game.Type==DOMINION.GAME.DAILY) {
+		this.GraphicsTool.DrawRectangle(60, 235, 125, 100, "white", 2);
+		this.GraphicsTool.DrawRectangle(70, 235, 50, 10, this.Specs.COLOUR, 0);
+		this.TextWriter.Write("Game", 75, 240, { COLOUR: "white" } );
+		if (this.PastGamesFlag) {
+			this.TextWriter.Write("Past", 70, 262, { COLOUR: "white", STYLE: FONT.STYLE.UNDERLINED } );
+			//-name of game chosen
+		} else {
+			this.TextWriter.Write("Daily", 70, 262, { COLOUR: "white", STYLE: FONT.STYLE.UNDERLINED } );
+			date = new Date();
+			date.getTime();
+			this.TextWriter.Write(date.toDateString(), 70, 286, { COLOUR: "white" } );
+		}
 	}
 
-	//Portalite jet
-	LeftJetSprites[1].Shapes.forEach(function(shape){shape.ResetSides(3);});
-//		this.ReColourSprite(LeftJetSprites[i], i);
-	x = 213 * ((i % 3)+0.5);
-	y = 213 * (Math.floor(i/3)+0.5);
-	LeftJetSprites[1].Draw(x, y);
+	//Game options
+	if (Game.Type==DOMINION.GAME.DAILY)
+		x = 90;
+	else
+		x = 0;
+	this.TextWriter.Write("Votes", 210+x, 240, { COLOUR: "white", STYLE: FONT.STYLE.UNDERLINED } );
+	this.TextWriter.Write("17", 220+x, 267, { COLOUR: "white" } );
+	this.TextWriter.Write("34", 220+x, 297, { COLOUR: "white" } );
+	this.TextWriter.Write("51", 220+x, 327, { COLOUR: "white" } );
 };
-DominionIntro.prototype.ReColourSprite = function(sprite, iPower) {
-	var i;
+DominionIntroView.prototype.OpenGlobalView = function() {  //UNLOGGED - probably REDUNDANT
 
-	for (i=0;i<sprite.Shapes.length;++i) {
-		if (sprite.Shapes[i].Colour=="grey")
-			sprite.Shapes[i].Colour = PowerColours[iPower][0];
-		else
-			sprite.Shapes[i].Colour = PowerColours[iPower][1];
-	}
+	GlobalView.SetNation(PlayerPower);
+	GlobalView.Open();
+	GlobalView.Update();
+};
+DominionIntroView.prototype.OpenAssetsView = function() {
+
+	++Game.Phase;
+	AssetsView.SetNation(PlayerPower);
+	WorldMap.SelectNation(PlayerPower);
+	AssetsView.Open();
 };
---
+++ dominion/office.js
@@ -0,0 +1,333 @@
+
+//TODO: Monitor banks switching back and forth between ministry status and satellite surveillance?
+//----------------------------------------------------
+//---------- DOMINION OFFICE VIEW --------------------
+var DominionOfficeView = function() {
+	var Nation;
+	var PrimaryColour, SecondaryColour;		//TODO: only useful if Allieds and City-States can be selected for display
+	var CalculatorImage, FolderImage, FolderLettersImage, GlobeImage,
+		 LaptopImage, NewspapersImage, PDAImage, PhoneImage;		//UNLOGGED
+	var Cabinet, Calendar, Chart, Clock;											//wall accessories
+	var Calculator, Folders, Globe, Laptop, Newspapers, PDA, Phone;		//desk accessories
+	//-budget pie chart, word games?, pic of leader?
+	var DeskTrapezoid;
+
+	var i, x, y;  //scratch variables
+};
+DominionOfficeView.prototype = new GenieView();
+DominionOfficeView.prototype.Set = function(cnvs, specs) {
+	GenieView.prototype.Set.call(this, cnvs, specs);
+
+	if (Game.CheckMobile())
+		this.SetMobileLocations();
+};
+DominionOfficeView.prototype.SetNation = function(nation) {
+
+	this.Nation = nation;
+	this.PrimaryColour = DominionUtils.GetPrimaryColour(this.Nation);
+	this.SecondaryColour = DominionUtils.GetSecondaryColour(this.Nation);
+	if (this.PrimaryColour==this.SecondaryColour)
+		this.SecondaryColour = "rgb(175,175,255)";
+};
+DominionOfficeView.prototype.SetImages = function() {
+
+	this.DeskTrapezoid = new GenieTrapezoid();
+	this.DeskTrapezoid.Set(this.Context, this.Specs.DESK);
+
+	this.CalculatorImage = new GenieImage();
+	this.CalculatorImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.CALCULATOR.IMAGE);
+	this.FolderImage = new GenieImage();
+	this.FolderImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.FOLDERS.IMAGE);
+	this.FolderLettersImage = new GenieImage();
+	this.FolderLettersImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.FOLDERS.IMAGE.LETTERS);
+	this.GlobeImage = new GenieImage();
+	this.GlobeImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.GLOBE.IMAGE);
+	this.LaptopImage = new GenieImage();
+	this.LaptopImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.LAPTOP.IMAGE);
+	this.NewspapersImage = new GenieImage();
+	this.NewspapersImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.NEWSPAPERS.IMAGE);
+	this.PDAImage = new GenieImage();
+	this.PDAImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.PDA.IMAGE);
+	this.PhoneImage = new GenieImage();
+	this.PhoneImage.Set(this.Context, ImageManager.Pics[IMAGeINDEX.OFFICE], this.Specs.PHONE.IMAGE);
+};
+DominionOfficeView.prototype.SetControls = function() {  //UNLOGGED
+};
+DominionOfficeView.prototype.SetComponents = function() {  //UNLOGGED
+
+	this.Cabinet = new FilingCabinet();
+	this.Cabinet.Set(this.Specs.CABINET, this.GraphicsTool);
+	this.Calculator = new OfficeCalculator();
+	this.Calculator.Set(this.Specs.CALCULATOR, this.CalculatorImage);
+	this.Calendar = new OfficeCalendar();
+	this.Calendar.Set(this.Specs.CALENDAR, this.GraphicsTool);
+	this.Clock = new OfficeClock();
+	this.Clock.Set(this.Specs.CLOCK, this.GraphicsTool);
+	this.Folders = new OfficeFolders();
+	this.Folders.Set(this.Specs.FOLDERS, this.FolderImage, this.FolderLettersImage);
+	this.Globe = new OfficePaperStash();
+	this.Globe.Set(this.Specs.GLOBE, this.GlobeImage);
+	this.Laptop = new OfficeLaptop();
+	this.Laptop.Set(this.Specs.LAPTOP, this.LaptopImage);
+	this.MonitorBank = new OfficeMonitorBank();
+	this.MonitorBank.Set(this.Specs.BANK, this.GraphicsTool);
+	this.Newspapers = new OfficePaperStash();
+	this.Newspapers.Set(this.Specs.NEWSPAPERS, this.NewspapersImage);
+	this.PDA = new OfficePaperStash();
+	this.PDA.Set(this.Specs.PDA, this.PDAImage);
+	this.Phone = new OfficePaperStash();
+	this.Phone.Set(this.Specs.PHONE, this.PhoneImage);
+	//-newspaper rack? diagonal shelves (wall mount)
+};
+DominionOfficeView.prototype.SetMobileLocations = function() {
+
+	this.Specs.WALL = this.Specs.WALL.MOBILE;
+	this.Specs.CARPET = this.Specs.CARPET.MOBILE;
+	this.Specs.DOOR = this.Specs.DOOR.MOBILE;
+	this.Specs.DOOrPANEL = this.Specs.DOOrPANEL.MOBILE;
+	this.Specs.KNOB = this.Specs.KNOB.MOBILE;
+};
+DominionOfficeView.prototype.Open = function() {
+
+	this.Specs.COLOUR = PowerColours[this.Nation.Index][1];
+
+	GenieView.prototype.Open.call(this);
+};
+/* TODO: either use this somewhere or subtract a LOG entry
+	SetPowerDisplays() {
+		var i;
+
+		for (i=0;i<this.PowerDisplays.length;++i) {
+	 this.PowerDisplays[i].Location = new Coordinate2D();
+	 this.PowerDisplays[i].Location.X = Math.floor(i/4)*750;
+	 this.PowerDisplays[i].Location.Y = 150*(i % 4);
+		}
+	},
+*/
+DominionOfficeView.prototype.Draw = function() {
+
+	//Wall
+	this.DrawRoom();
+	this.Clock.Draw();
+	this.Cabinet.Draw();
+	this.Calendar.Draw();
+//	this.Chart.Draw();
+	this.MonitorBank.Draw();
+
+	//Desk
+	this.DrawDesk();
+	this.DrawDeskContents();
+
+	//TEMP ticker
+	this.GraphicsTool.DrawRectangle(0, 0, SCREEN.WIDTH, 28, "red", 0);
+	this.GraphicsTool.DrawRectangle(0, 0, SCREEN.WIDTH, 28, "black", 3);
+};
+DominionOfficeView.prototype.DrawRoom = function() {
+
+	//Background
+	this.GraphicsTool.DrawRectangle(0, 0, SCREEN.WIDTH, this.Specs.WALL.H, this.PrimaryColour, 0);		//wall
+	this.GraphicsTool.DrawRectangle(0, this.Specs.CARPET.T, SCREEN.WIDTH, this.Specs.CARPET.H, this.SecondaryColour, 0);		//carpet
+
+	//Door
+	this.GraphicsTool.DrawRectangle(this.Specs.DOOR.L, this.Specs.DOOR.T, this.Specs.DOOR.W, this.Specs.DOOR.H, DOMINION.COLOUR.OFFICE.EDGE, 3);
+	this.GraphicsTool.DrawRectangle(this.Specs.DOOR.L+3, this.Specs.DOOR.T+3, this.Specs.DOOR.W-6, this.Specs.DOOR.H-3, DOMINION.COLOUR.OFFICE.WOOD, 0);
+	this.GraphicsTool.DrawRoundedRectangle(this.Specs.DOOrPANEL.L, this.Specs.DOOrPANEL.T, this.Specs.DOOrPANEL.W, this.Specs.DOOrPANEL.H, 
+															this.Specs.DOOrPANEL.R, DOMINION.COLOUR.OFFICE.EDGE, DOMINION.COLOUR.OFFICE.DOOR.PANEL, 1);
+	this.GraphicsTool.DrawCircle(this.Specs.KNOB.X, this.Specs.KNOB.Y, this.Specs.KNOB.R, DOMINION.COLOUR.OFFICE.EDGE, 0);
+};
+DominionOfficeView.prototype.DrawCabinet = function() {  //UNLOGGED
+/*
+var CABINEtSPRITE =  { W: 115, H: 230, GS: [ [SHAPE.RECTANGLE, GREY.SILVER, 0, [0,-230,115,230] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 1, [5,-225,105,70] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 0, [45,-210,25,10] ],
+					     [SHAPE.RECTANGLE, "black", 1, [45,-210,25,10] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 1, [5,-150,105,70] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 0, [45,-135,25,10] ],
+					     [SHAPE.RECTANGLE, "black", 1, [45,-135,25,10] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 1, [5,-75,105,70] ],
+					     [SHAPE.RECTANGLE, GREY.ASH, 0, [45,-60,25,10] ],
+					     [SHAPE.RECTANGLE, "black", 1, [45,-60,25,10] ] ]
+};  //TODO: add dark top trapezoid
+*/
+};
+DominionOfficeView.prototype.DrawMonitorBank = function() {
+		var i;
+
+//		this.MonitorBank.forEach(function(monitor) {monitor.Draw();});
+/* */
+		for (i=0;i<MINISTRY.PORTFOLIOS;++i)
+	 if (i<MINISTRY.PORTFOLIOS/2)
+		 this.GraphicsTool.DrawRoundedRectangle(183+(130*i), 50, 120, 120, 10, "black", "white", 5);
+	 else
+		 this.GraphicsTool.DrawRoundedRectangle(183+(130*(i-4)), 180, 120, 120, 10, "black", "white", 5);
+/* */
+
+		//NOTE: Scratchpad is used for drawing the monitor contents, and can't be used for any other purpose
+};
+/* TODO: either use this somewhere or subtract a LOG entry
+DominionOfficeView.prototype.DrawMiniScreens = function() {  //1 for each rival mini-power
+		var i;
+
+		this.GraphicsTool.DrawRectangle(0, 0, 150, 600, "dodgerblue", 0);
+		this.GraphicsTool.DrawRectangle(750, 0, 150, 600, "dodgerblue", 0);
+		for (i=0;i<(POWER.COUNT-1)/2;++i) {
+	 this.GraphicsTool.DrawRectangle(0, 150*i, 150, 150, "black", 3);
+	 this.GraphicsTool.DrawRectangle(750, 150*i, 150, 150, "black", 3);
+		}
+
+		//TODO: depending on state, draw sashed octagon/vital stats/current activity
+};
+*/
+DominionOfficeView.prototype.DrawDesk = function() {  //UNLOGGED - adjust for desktop/mobile
+
+	this.GraphicsTool.DrawRectangle(0, 390, SCREEN.WIDTH, 10, DOMINION.COLOUR.OFFICE.WOOD, 0);
+	this.DeskTrapezoid.Colour = DOMINION.COLOUR.OFFICE.WOOD;
+	this.DeskTrapezoid.LineWidth = 0;
+	this.DeskTrapezoid.QuickDraw();
+	this.DeskTrapezoid.Colour = DOMINION.COLOUR.OFFICE.EDGE;
+	this.DeskTrapezoid.LineWidth = 3;
+	this.DeskTrapezoid.QuickDraw();
+};
+DominionOfficeView.prototype.DrawDeskContents = function() {  //UNLOGGED
+
+	this.Calculator.Draw();
+	this.Folders.Draw();
+	this.Laptop.Draw();
+	this.Newspapers.Draw();
+	this.Calculator.Draw();	//accounting calculator which opens budget view
+	this.Phone.Draw();
+	this.Globe.Draw();
+	this.PDA.Draw();
+		//globe - to get map, which itself will have multiple depths and office wide/full screen optional modes
+		//calendar - first 3 letters of each Day entry can be exculpated to give necessary abbreviation for month
+
+		//power columns (can be switched to ally columns - most relevant ones will be shown)
+		//monitor bank
+		//chair - ministers enter via door, sit in chair (or maybe chief minister/adviser does when situation critical)
+		//popularity rating chart, type depending on government type
+};
+/* all below will either be used elsewhere or simply UNLOGGED
+	DrawCityState(size, x, y) {
+		var i;
+		var octagon;
+		var offset;
+
+		octagon = DominionCalcPad.GetOctagonVertices(size);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x, (SCREEN.HEIGHT/2)+y, octagon, "green", 0);
+		//NOTE: inner 4 octagons are centred size*sin(45),size*cos(45) NE,SE,SW,NW
+		offset = size*Math.sin(Utilities.DegreesToRadians(45));
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x+offset, (SCREEN.HEIGHT/2)+y-offset, octagon, "chartreuse", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x+offset, (SCREEN.HEIGHT/2)+y+offset, octagon, "chartreuse", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x-offset, (SCREEN.HEIGHT/2)+y+offset, octagon, "chartreuse", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x-offset, (SCREEN.HEIGHT/2)+y-offset, octagon, "chartreuse", 0);
+		//NOTE: outer 4 octagons are centred (size + size*tan(22.5)) N,S,E,W
+		offset = size*(1 + Math.tan(Utilities.DegreesToRadians(22.5)));  //NOTE: this is how to calculate size of square
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x, (SCREEN.HEIGHT/2)+y-offset, octagon, "yellow", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x+offset, (SCREEN.HEIGHT/2)+y, octagon, "yellow", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x, (SCREEN.HEIGHT/2)+y+offset, octagon, "yellow", 0);
+		this.GraphicsTool.DrawPolygonFromVertices((SCREEN.WIDTH/2)+x-offset, (SCREEN.HEIGHT/2)+y, octagon, "yellow", 0);
+	},
+	DrawState(size) {
+		var location, location_a, location_b;
+
+		this.Screen.clearRect(150, 0, 600, 600);
+		this.DrawCityState(size, 0, 0);
+		location = 3*size*Math.sin(Utilities.DegreesToRadians(45));
+		this.DrawCityState(size, location, -location);
+		this.DrawCityState(size, location, location);
+		this.DrawCityState(size, -location, location);
+		this.DrawCityState(size, -location, -location);
+		location = 3*size*(1 + Math.tan(Utilities.DegreesToRadians(22.5)));
+		this.DrawCityState(size, 0, -location);
+		this.DrawCityState(size, location, 0);
+		this.DrawCityState(size, 0, location);
+		this.DrawCityState(size, -location, 0);
+		location_a = 2*size*(1 + Math.tan(Utilities.DegreesToRadians(22.5)));
+		location_b = 2*location_a;
+		this.DrawCityState(size, location_a, -location_b);
+		this.DrawCityState(size, location_b, -location_a);
+		this.DrawCityState(size, location_b, location_a);
+		this.DrawCityState(size, location_a, location_b);
+		this.DrawCityState(size, -location_a, location_b);
+		this.DrawCityState(size, -location_b, location_a);
+		this.DrawCityState(size, -location_b, -location_a);
+		this.DrawCityState(size, -location_a, -location_b);
+	},
+	PlayRound() {
+	},
+	Play() {
+		var tLength;	//t- text
+
+		this.AnimationFrameHandle = requestAnimationFrame(this.Play.bind(this));
+
+		//Draw power octagons
+		for (this.i=0;this.i<POWER.COUNT-1;++this.i) {
+	 this.x = this.PowerDisplays[this.i].Location.X + (150/2);  //ISSUE: 150/2 maybe should become OFFICE.MINiSCREEnHALF
+	 this.y = this.PowerDisplays[this.i].Location.Y + (150/2);
+	 this.GraphicsTool.DrawCircle(this.x, this.y, (150-10)/2, PowersInfo[this.i][POWErINDEX.COLOUR1], 0);
+	 tLength = this.Screen.measureText(PowersInfo[this.i][0]).width;
+	 this.TextWriter.Write(PowersInfo[this.i][0], Math.round(this.PowerDisplays[this.i].Location.X+(150/2)-(tLength/2)), this.PowerDisplays[this.i].Location.Y+(150/2));
+		}
+
+		//TODO: monitor bank views can be switched between ministry info and satellite surveillance, switch on desk
+
+		if (OfficeRoundButton.CheckClicked()) {
+	 cancelAnimationFrame(this.AnimationFrameHandle);
+	 this.PlayRound();
+		}
+
+		if (Mouse.CheckLeftClicked()) {
+	 if ((Mouse.ClickX>298 && Mouse.ClickX<602) && (Mouse.ClickY>465 && Mouse.ClickY<551))
+		 cancelAnimationFrame(this.AnimationFrameHandle);
+	 switch (true) {
+		 case ((Mouse.ClickX>300 && Mouse.ClickX<396) && (Mouse.ClickY>467 && Mouse.ClickY<486)):
+//			 LandTheatre.Set(this.Screen, this.InfoBox, this.GraphicsTool);
+//			 LandTheatre.Play();
+			 LandTheatre.SetSimpleBattle();
+			 LandTheatre.PlaySimpleBattle();
+			 break;
+		 case ((Mouse.ClickX>300 && Mouse.ClickX<396) && (Mouse.ClickY>488 && Mouse.ClickY<507)):
+//			 beachead
+			 break;
+		 case ((Mouse.ClickX>300 && Mouse.ClickX<396) && (Mouse.ClickY>509 && Mouse.ClickY<528)):
+//			 naval theatre
+			 break;
+		 case ((Mouse.ClickX>300 && Mouse.ClickX<396) && (Mouse.ClickY>530 && Mouse.ClickY<549)):
+//			 submarine
+			 break;
+		 case ((Mouse.ClickX>402 && Mouse.ClickX<498) && (Mouse.ClickY>467 && Mouse.ClickY<486)):
+//			 dogfight
+			 break;
+		 case ((Mouse.ClickX>402 && Mouse.ClickX<498) && (Mouse.ClickY>488 && Mouse.ClickY<507)):
+//			 air mission
+			 break;
+		 case ((Mouse.ClickX>402 && Mouse.ClickX<498) && (Mouse.ClickY>509 && Mouse.ClickY<528)):
+//			 champions duel
+			 break;
+		 case ((Mouse.ClickX>402 && Mouse.ClickX<498) && (Mouse.ClickY>530 && Mouse.ClickY<549)):
+//			 interdiction
+			 break;
+		 case ((Mouse.ClickX>504 && Mouse.ClickX<600) && (Mouse.ClickY>467 && Mouse.ClickY<486)):
+//			 air raid
+			 break;
+		 case ((Mouse.ClickX>504 && Mouse.ClickX<600) && (Mouse.ClickY>488 && Mouse.ClickY<507)):
+//			 sea raid
+			 break;
+		 case ((Mouse.ClickX>504 && Mouse.ClickX<600) && (Mouse.ClickY>509 && Mouse.ClickY<528)):
+//			 land raid
+			 break;
+		 case ((Mouse.ClickX>504 && Mouse.ClickX<600) && (Mouse.ClickY>530 && Mouse.ClickY<549)):
+//			 missile base
+			 break;
+	 }
+
+	 if ((Mouse.ClickX>350 && Mouse.ClickX<550) && (Mouse.ClickY>350 && Mouse.ClickY<410)) {
+		 cancelAnimationFrame(this.AnimationFrameHandle);
+		 var tGame = new TextMiniGame();
+		 tGame.Set(this.Screen, this.GraphicsTool, this.TextWriter);
+		 tGame.Play();
+	 }
+		}
+	}
+};
+*/
