eed078eda20d14e5d319b01d1972f7e175be1db2
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-08-30 22:19:43 +0500

    Add files via upload

---
+++ dominion/data/seeds.js
@@ -0,0 +1,8 @@
+
+var Daily = [ [ 449308668, "Male", "Female", "Significant theme/event" ]
+];
+
+//Bumbershoot - building and exporting them w/ some uniquely available material may turn an Allied or Power very rich, and become a threat;
+//					 counter options are blockading portals to prevent export, capturing the province where the material is mined, somehow manufacturing
+//					 same products somehow, or authorize sabotage of manufacturing facilities
+//Lingonberry
