5f4cfdbf2f000fba2b02a56e3c1a079d718e0c26
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-11-08 22:51:53 +0500

    Add files via upload

--- lexitrix/views/fiddle/solutions10.js
+++ lexitrix/views/fiddle/solutions10.js
@@ -31,5 +31,72 @@ var FiddleSolutions1000 = [ [ "epoxy", "enact", "ocean", "yodel", "ahead", "tona
 									 [ "steam", "slept", "exalt", "muted", "enact", "toted" ],
 									 [ "power", "piano", "whirl", "ruler", "axial", "oiler" ],
 									 [ "adieu", "avert", "inane", "utter", "exact", "their" ],		//932
-									 [ "gloom", "ghost", "owned", "mural", "owner", "tidal" ]
-];  //933
+									 [ "gloom", "ghost", "owned", "mural", "owner", "tidal" ],
+									 [ "snide", "spare", "issue", "elegy", "aisle", "every" ],
+									 [ "ember", "ended", "brawn", "runny", "drawn", "dinky" ],
+									 [ "shave", "sling", "amuse", "elect", "inure", "great" ],		//936
+									 [ "apart", "align", "aback", "tweed", "inane", "nuked" ],
+									 [ "cocky", "couch", "cabal", "yearn", "umbra", "halon" ],
+									 [ "booze", "bliss", "otter", "error", "inter", "sorer" ],
+									 [ "never", "noted", "villa", "runny", "talon", "diary" ],		//940
+									 [ "night", "niche", "graft", "tuner", "chain", "enter" ],
+									 [ "druid", "dumps", "udder", "duchy", "medic", "sorry" ],
+									 [ "preen", "patch", "eater", "nanny", "titan", "hurry" ],
+									 [ "grist", "gated", "ivory", "tweed", "trope", "dryad" ],		//944
+									 [ "beige", "brave", "icing", "ensue", "amiss", "eagle" ],
+									 [ "clear", "clasp", "exile", "route", "adieu", "piece" ],
+									 [ "bluff", "bloom", "utter", "flout", "outdo", "merit" ],
+									 [ "syrup", "sepia", "round", "prone", "pluto", "addle" ],		//948
+									 [ "gaunt", "gamut", "until", "tread", "mitre", "tiled" ],
+									 [ "evade", "elite", "await", "elegy", "irate", "entry" ],
+									 [ "anvil", "awake", "viola", "light", "along", "enact" ],
+									 [ "peach", "petal", "ambit", "harsh", "tuber", "latch" ],		//952
+									 [ "trove", "tramp", "olden", "epoxy", "audio", "penny" ],
+									 [ "third", "twang", "idiot", "dryly", "amity", "gutsy" ],
+									 [ "vixen", "voter", "yeast", "nests", "toads", "rites" ],
+									 [ "fifty", "flail", "frown", "yearn", "aroma", "linen" ],		//956
+									 [ "moral", "mover", "radon", "leach", "vodka", "ranch" ],
+									 [ "silky", "scant", "lease", "yummy", "alarm", "teeny" ],
+									 [ "cream", "crawl", "extra", "marry", "aster", "leaky" ],
+									 [ "chirp", "carol", "imbue", "perky", "rebar", "leery" ],		//960
+									 [ "cagey", "cadet", "grant", "yokel", "drank", "total" ],
+									 [ "hazel", "heart", "zebra", "liege", "amble", "trace" ],
+									 [ "paved", "primp", "vista", "ditch", "inset", "peach" ],
+									 [ "ogled", "odour", "lanky", "dwell", "ounce", "royal" ],		//964
+									 [ "choke", "cubic", "owner", "ensue", "bonus", "curve" ],
+									 [ "genre", "greet", "noble", "ended", "embed", "tweed" ],
+									 [ "preen", "piper", "event", "nutty", "pleat", "retry" ],
+									 [ "cleat", "crumb", "erupt", "tipsy", "usurp", "batty" ],		//968
+									 [ "uncut", "usual", "cadet", "there", "undue", "lathe" ],
+									 [ "duvet", "duchy", "venue", "tread", "canoe", "yield" ],
+									 [ "fumes", "focal", "mated", "sidle", "cited", "ladle" ],
+									 [ "chunk", "crane", "udder", "kinky", "addon", "early" ],		//972
+									 [ "thong", "trout", "octet", "goose", "outdo", "tithe" ],
+									 [ "occur", "ocean", "craft", "retro", "enact", "nitro" ],
+									 [ "reedy", "rains", "equal", "yield", "inure", "solid" ],
+									 [ "boost", "burst", "order", "tubby", "redub", "tardy" ],		//976
+									 [ "wrest", "wharf", "eerie", "tally", "aural", "fiery" ],
+									 [ "raise", "resin", "idiot", "eyrie", "skier", "nitre" ],
+									 [ "mucus", "meter", "crick", "shred", "trier", "raked" ],
+									 [ "surge", "scald", "right", "entry", "argot", "dotty" ],		//980
+									 [ "hence", "hasty", "nurse", "ended", "sired", "yield" ],
+									 [ "group", "grown", "overt", "penny", "ocean", "nutty" ],
+									 [ "march", "mused", "reign", "horse", "skier", "dance" ],
+									 [ "trail", "tuner", "assay", "ladle", "nosed", "rhyme" ],		//984
+									 [ "month", "muted", "nears", "husky", "traps", "dusty" ],
+									 [ "creed", "cairn", "eject", "ditto", "inept", "nitro" ],
+									 [ "spate", "salvo", "apron", "erase", "larva", "ounce" ],
+									 [ "croft", "cabin", "onion", "taffy", "brief", "nanny" ],		//988
+									 [ "snide", "stave", "idler", "ended", "ailed", "erred" ],
+									 [ "neigh", "nasal", "inert", "hiker", "steak", "later" ],
+									 [ "spear", "spoil", "event", "ratio", "overt", "lotto" ],
+									 [ "taboo", "tired", "being", "opera", "raise", "dogma" ],		//992
+									 [ "karma", "knave", "rider", "antsy", "audit", "early" ],
+									 [ "spike", "sheer", "inane", "ensue", "exams", "reeve" ],
+									 [ "stamp", "slant", "alike", "pause", "adieu", "theme" ],
+									 [ "eagle", "eight", "grape", "erect", "grate", "tweet" ],		//996
+									 [ "flout", "flask", "organ", "tarry", "augur", "kinky" ],
+									 [ "plush", "pylon", "ulcer", "halve", "local", "nurse" ],
+									 [ "heave", "hotel", "anode", "empty", "troop", "leery" ],
+									 [ "fauna", "foist", "union", "algae", "icing", "tonne" ]
+];  //1000
