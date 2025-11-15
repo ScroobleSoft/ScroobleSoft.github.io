3199d21410942a49aa0da285afdc92ffbfcbaf1f
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-11-15 22:58:01 +0500

    Add files via upload

--- dominion/alliances/alliance.js
+++ dominion/alliances/alliance.js
@@ -2,17 +2,24 @@
 //-------------------------------------------------
 //---------- DOMINION ALLIANCE --------------------  TODO: could move to Types.js if remains basic
 var DominionAlliance = function() {
-	var Type;
+	var Randomizer;
+	var Type, Commodity;
 	var Power, AlliedState;
 };
 DominionAlliance.prototype = {
-	Set(power, allied, type) {
+	Set(power, allied, type, rGenerator) {
+		this.Randomizer = rGenerator;
 		this.Power = power;
 		this.AlliedState = allied;
 		this.Type = type;
-		this.UpdateHistory();
+		if (this.Type==ALLIANCE.GRANT)
+			this.SetCommodity();
 	},
-	UpdateHistory() {
+	SetCommodity() {
+
+		this.Commodity = PowerProfiles[this.AlliedState.AssociatedIndex][1];
+	},
+	UpdateHistory() {  //TODO: re-write, rename ::Update
 
 		switch (this.Type) {
 			case ALLIANCE.GRANT:
