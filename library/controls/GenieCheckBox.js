08a88ccd3b1002410b3570a73fcbf7f8c4534a03
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-12-06 20:31:35 +0500

    Add files via upload

--- library/controls/GenieCheckBox.js
+++ library/controls/GenieCheckBox.js
@@ -9,13 +9,21 @@ GenieCheckBox.prototype.Set = function(canvas, specs, img) {
 	GenieControl.prototype.Set.call(this, canvas, specs, img);
 
 	this.Checked = false;
+	if (this.Specs.LABEL)
+		this.SetClickBox();
+};
+GenieCheckBox.prototype.SetClickBox = function() {
+
+	this.Specs.W += 3 + Math.round(this.Context.measureText(this.Specs.LABEL).width);
 };
 GenieCheckBox.prototype.Draw = function() {
+
 	this.DrawBox();
-	this.TextWriter.Write(this.Specs.LABEL, this.Specs.L+this.Specs.W+3, this.Specs.T+this.Specs.H);
+	this.TextWriter.Write(this.Specs.LABEL, this.Specs.L+this.Pic.Specs.PATCH.W+3, this.Specs.T+this.Specs.H-2);
 };
 GenieCheckBox.prototype.DrawBox = function() {
-	this.Context.clearRect(this.Specs.L, this.Specs.T, this.Specs.W, this.Specs.H);
+
+	this.Context.clearRect(this.Specs.L, this.Specs.T, this.Pic.Specs.PATCH.W, this.Specs.H);
 	if (this.Checked)
 		this.Pic.DrawPatchNumber(1, this.Specs.L, this.Specs.T);
 	else
@@ -33,4 +41,7 @@ GenieCheckBox.prototype.UnCheck = function() {
 	this.Checked = false;
 	this.DrawBox();
 };
+GenieCheckBox.prototype.CheckChecked = function() {
 
+	return (this.Checked);
+};
