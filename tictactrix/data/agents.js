728f1ee8b1430180f58c052e79a5f3590bb00952
ScroobleSoft <131161503+ScroobleSoft@users.noreply.github.com>
2025-12-06 19:32:33 +0500

    Add files via upload

--- tictactrix/data/agents.js
+++ tictactrix/data/agents.js
@@ -2,27 +2,27 @@
 var TACTICAlUNIT = { GUNNER: 0, BAZOOKER: 1, MISSILER: 2,
 							JEEP: 3, ROCKEtPOD: 4, MISSILeLAUNCHER: 5, HOWITZER: 6, ARTILLERY: 7, AV: 8, SWIFtTANK: 9, HYBRIdTANK: 10, MEGaTANK: 11,
 							FRIGATE: 12, CRUISER: 13, DESTROYER: 14, BATTLESHIP: 15, HELICOPTER: 16, STRAFER: 17, FIGHTER: 18, BOMBER: 19, TYPES: 20,
-							TYPE: { LAND: 21, SEA: 22, AIR: 23 }, VARIETIES: { LAND: 12, SEA: 4, AIR: 4 }
+							TYPE: { LAND: 21, SEA: 22, AIR: 23 }, VARIETIES: { LAND: 12, SEA: 4, AIR: 4, PORTLESS: 16 }
 };
 var TROOPER = { SHIELD: 2, RELOAD: 4,
 					 ARM: { E: { X: 4, Y: -8 }, W: { X:  7, Y: -8 } }, FEET: { E: { X: 3, Y: 3 }, W: { X: 2, Y: 3 } }, OFFSET: { X: 12, Y: 34 } };
-var GUNNER = { POTENCY: "C-", GUN: { E: { X: 4, Y:  -6 }, W: { X: -2, Y: -6 } } };
-var BAZOOKER = { POTENCY: "C", BAZOOKA: { E: { X: -3, Y: -8 }, W: { X: -4, Y: -8 } } };
-var MISSILER = { POTENCY: "C+", LAUNCHER: { E: { X: -4, Y: -9 }, W: { X: 0, Y: -9 } } };
-var JEEP = { POTENCY: "B-", WHEEL: { B: { X: 4, Y: 7 }, F: { X: 23, Y: 7 } } };
-var ROCKEtPOD = { POTENCY: "B", WHEEL: { L: { X: 1, Y: 10 }, C: { X: 13, Y: 10 }, R: { X: 25, Y: 10 } } };
-var MISSILeLAUNCHER = { POTENCY: "B+", WHEEL: { L: { X: 1, Y: 10 }, C: { X: 13, Y: 10 }, R: { X: 25, Y: 10 } } };
-var HOWITZER = {};
-var ARTILLERY = {};
-var AV = {};
-var SWIFtTANK = {};
-var HYBRIdTANK = {};
-var MEGaTANK = {};
-var FRIGATE = {};
-var CRUISER = {};
-var DESTROYER = {};
-var BATTLESHIP = {};
-var HELICOPTER = {};
-var STRAFER = {};
-var FIGHTER = {};
-var BOMBER = {};
+var GUNNER = { POTENCY: "D-", GUN: { E: { X: 4, Y:  -6 }, W: { X: -2, Y: -6 } } };
+var BAZOOKER = { POTENCY: "D", BAZOOKA: { E: { X: -3, Y: -8 }, W: { X: -4, Y: -8 } } };
+var MISSILER = { POTENCY: "D+", LAUNCHER: { E: { X: -4, Y: -9 }, W: { X: 0, Y: -9 } } };
+var JEEP = { POTENCY: "C-", WHEEL: { B: { X: 4, Y: 5 }, F: { X: 23, Y: 5 } }, OFFSET: { X: 1, Y: 32 } };
+var ROCKEtPOD = { POTENCY: "C", WHEEL: { L: { X: 1, Y: 9 }, C: { X: 13, Y: 9 }, R: { X: 25, Y: 9 } }, OFFSET: { X: 1, Y: 28 } };
+var MISSILeLAUNCHER = { POTENCY: "C+", WHEEL: { L: { X: 1, Y: 9 }, C: { X: 13, Y: 9 }, R: { X: 25, Y: 9 } }, OFFSET: { X: 1, Y: 28 } };
+var HOWITZER = { POTENCY: "B-", TREAD: { E: { X: -4, Y: 6 }, W: { X: 7, Y: 6 } }, OFFSET: { X: 3, Y: 31 } };
+var ARTILLERY = { POTENCY: "B", TREAD: { E: { X: -4, Y: 6 }, W: { X: -1, Y: 6 } }, OFFSET: { X: 2, Y: 31 } };
+var AV = { POTENCY: "B+", TREAD: { X: -2, Y: 6 }, OFFSET: { X: 1, Y: 31 } };
+var SWIFtTANK = { POTENCY: "A-", TRACK: { X: 0, Y: 7 }, OFFSET: { X: 6, Y: 30 } };
+var HYBRIdTANK = { POTENCY: "A", TRACK: { X: -1, Y: 7 }, OFFSET: { X: 2, Y: 30 } };
+var MEGaTANK = { POTENCY: "A+", TRACK: { E: { X: 0, Y: 7 }, W: { X: 5, Y: 7 } }, OFFSET: { X: 2, Y: 30 } };
+var FRIGATE = { OFFSET: { X: 2, Y: 37 } };
+var CRUISER = { OFFSET: { X: 2, Y: 37 } };
+var DESTROYER = { OFFSET: { X: 2, Y: 37 } };
+var BATTLESHIP = { OFFSET: { X: 1, Y: 37 } };
+var HELICOPTER = { OFFSET: { X: 1, Y: 37 } };
+var STRAFER = { OFFSET: { X: 1, Y: 37 } };
+var FIGHTER = { OFFSET: { X: 1, Y: 37 } };
+var BOMBER = { OFFSET: { X: 1, Y: 37 } };
---
+++ tictactrix/data/sprites.js
@@ -0,0 +1,83 @@
+
+//Troopers
+var EAStTROOPErSPRITE = { L: 1, T: 1, W: 15, H: 25, O: 2 };
+var WEStTROOPErSPRITE = { L: 1, T: 28, W: 15, H: 25, O: 2 };
+var FEEtSPRITE = { L: 1, T: 55, W: 10, H: 3, O: 2 };
+var RIGHtARmSPRITE = { L: 137, T: 1, W: 5, H: 3 };
+var LEFtARmSPRITE = { L: 137, T: 6, W: 5, H: 3 };
+
+//Weapons
+var EAStRIFLeSPRITE = { L: 144, T: 1, W: 13, H: 5 };
+var WEStRIFLeSPRITE = { L: 144, T: 8, W: 13, H: 5 };
+var EAStBAZOOKaSPRITE = { L: 137, T: 15, W: 22, H: 8 };
+var WEStBAZOOKaSPRITE = { L: 137, T: 25, W: 22, H: 8 };
+var EAStLAUNCHErSPRITE = { L: 137, T: 35, W: 20, H: 9 };
+var WEStLAUNCHErSPRITE = { L: 137, T: 46, W: 20, H: 9 };
+
+//Light vehicles
+var EAStJEEpSPRITE = { L: 1, T: 74, W: 38, H: 26, O: 2 };
+var WEStJEEpSPRITE = { L: 321, T: 74, W: 38, H: 26, O: 2 };
+var EAStROCKEtPOdSPRITE = { L: 1, T: 102, W: 37, H: 23, O: 2 };
+var WEStROCKEtPOdSPRITE = { L: 313, T: 102, W: 37, H: 23, O: 2 };
+var EAStMISSILeLAUNChSPRITE = { L: 1, T: 127, W: 37, H: 23, O: 2 };
+var WEStMISSILeLAUNChSPRITE = { L: 313, T: 127, W: 37, H: 23, O: 2 };
+
+//Medium vehicles
+var EAStHOWITZErSPRITE = { L: 1, T: 152, W: 31, H: 18, O: 2 };
+var WEStHOWITZErSPRITE = { L: 265, T: 152, W: 31, H: 18, O: 2 };
+var EAStARTILLERySPRITE = { L: 1, T: 172, W: 31, H: 18, O: 2 };
+var WEStARTILLERySPRITE = { L: 265, T: 172, W: 31, H: 18, O: 2 };
+var EAStAVsPRITE = { L: 1, T: 192, W: 34, H: 27, O: 2 };
+var WEStAVsPRITE = { L: 289, T: 192, W: 34, H: 27, O: 2 };
+
+//Heavy vehicles
+var EAStSWIFtTANkSPRITE = { L: 1, T: 221, W: 28, H: 16, O: 2 };
+var WEStSWIFtTANkSPRITE = { L: 241, T: 221, W: 28, H: 16, O: 2 };
+var EAStHYBRIdTANkSPRITE = { L: 1, T: 239, W: 33, H: 19, O: 2 };
+var WEStHYBRIdTANkSPRITE = { L: 281, T: 239, W: 33, H: 19, O: 2 };
+var EAStMEGaTANkSPRITE = { L: 1, T: 260, W: 36, H: 20, O: 2 };
+var WEStMEGaTANkSPRITE = { L: 305, T: 260, W: 36, H: 20, O: 2 };
+
+//Undercarriage
+var TIReSPRITE = { L: 159, T: 2, W: 11, H: 11, O: 2 };
+var SMALlTREAdSPRITE = { L: 161, T: 15, W: 28, H: 6, O: 2 };
+var MEDIUmTREAdSPRITE = { L: 161, T: 23, W: 36, H: 6, O: 2 };
+var LARGeTREAdSPRITE = { L: 161, T: 31, W: 38, H: 6, O: 2 };
+var SWIFtTRACkSPRITE = { L: 161, T: 39, W: 28, H: 7, O: 2 };
+var HYBRIdTRACkSPRITE = { L: 161, T: 48, W: 35, H: 7, O: 2 };
+var MEGaTRACkSPRITE = { L: 161, T: 57, W: 31, H: 7, O: 2 };
+
+//Ships
+var EAStFRIGATeSPRITE = { L: 1, T: 381, W: 36, H: 20, O: 2 };
+var WEStFRIGATeSPRITE = { L: 305, T: 381, W: 36, H: 20, O: 2 };
+var EAStCRUISErSPRITE = { L: 1, T: 403, W: 38, H: 19, O: 2 };
+var WEStCRUISErSPRITE = { L: 321, T: 403, W: 38, H: 19, O: 2 };
+var EAStDESTROYErSPRITE = { L: 1, T: 424, W: 36, H: 27, O: 2 };
+var WEStDESTROYErSPRITE = { L: 305, T: 424, W: 36, H: 27, O: 2 };
+var EAStBATTLESHIpSPRITE = { L: 1, T: 453, W: 38, H: 28, O: 2 };
+var WEStBATTLESHIpSPRITE = { L: 321, T: 453, W: 38, H: 28, O: 2 };
+
+//Jets
+var EAStFIGHTErSPRITE = { L: 1, T: 282, W: 38, H: 21, O: 2 };
+var WEStFIGHTErSPRITE = { L: 321, T: 282, W: 38, H: 21, O: 2 };
+var EAStBOMBErSPRITE = { L: 1, T: 305, W: 38, H: 22, O: 2 };
+var WEStBOMBErSPRITE = { L: 321, T: 305, W: 38, H: 22, O: 2 };
+var EAStSTRAFErSPRITE = { L: 1, T: 329, W: 38, H: 18, O: 2 };
+var WEStSTRAFErSPRITE = { L: 321, T: 329, W: 38, H: 18, O: 2 };
+var EAStHELICOPTErSPRITE = { L: 1, T: 349, W: 38, H: 30, O: 2 };
+var WEStHELICOPTErSPRITE = { L: 321, T: 349, W: 38, H: 30, O: 2 };
+
+//Trooper ammo
+var BULLEtSPRITE = { L: 98, T: 55, W: 3, H: 3, O: 2 };
+var SHELlSPRITE = { L: 1, T: 67, W: 5, H: 5, O: 2 };
+var EAStMISSILeHEAdSPRITE = { L: 1, T: 60, W: 7, H: 5, O: 2 };
+var WEStMISSILeHEAdSPRITE = { L: 73, T: 60, W: 7, H: 5, O: 2 };
+
+//Large ammo
+var EAStAAMsPRITE = { L: 319, T: 1, W: 15, H: 4, O: 2 };
+var WEStAAMsPRITE = { L: 319, T: 7, W: 15, H: 4, O: 2 };
+var EAStBOMbSPRITE = { L: 281, T: 38, W: 9, H: 4, O: 2 };
+var WEStBOMbSPRITE = { L: 369, T: 38, W: 9, H: 4, O: 2 };
+var EAStMISSILeSPRITE = { L: 281, T: 14, W: 18, H: 10, O: 2 };
+var WEStMISSILeSPRITE = { L: 281, T: 26, W: 18, H: 10, O: 2 };
+var MINeSPRITE = { L: 211, T: 2, W: 10, H: 10, O: 2 };
